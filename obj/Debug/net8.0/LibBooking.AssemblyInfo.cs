2ce4dcc030e331851a882730178403c502328b85
Vyom <vyom08@gmail.com>
2024-08-26 19:06:54 +1200

    Updated asp-action for Home

--- obj/Debug/net8.0/LibBooking.AssemblyInfo.cs
+++ obj/Debug/net8.0/LibBooking.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibBooking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2f21b3eee3763c526a5da1551826abe9c74dc09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61306af45dcf657f6543deb61495afb0daa918cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibBooking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibBooking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
